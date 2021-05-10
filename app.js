e1690f1645073dcc2f5243b4a71b9a6d76c25825
Kevin Kunkel <Christopher.Kunkel@traviscountytx.gov>
2021-05-10 13:33:39 -0500

    feature/questions

--- app.js
+++ app.js
@@ -5,17 +5,46 @@ const promptUser = () => {
     {
       type: "input",
       name: "name",
-      message: "What is your name?",
+      message: "What is your name? (Required)",
+      validate: nameInput => {
+          if (nameInput){
+              return true;
+          } else {
+              console.log('Please enter your name!');
+              return false;
+          }
+      }
     },
     {
       type: "input",
       name: "github",
-      message: "Enter your GitHub Username",
+      message: "Enter your GitHub Username (Required)",
+      validate: githubInput => {
+        if (githubInput){
+            return true;
+        } else {
+            console.log('Please enter your GitHub username!');
+            return false;
+        }
+    }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some information about yourself for an "About" section?',
+        default: true,
     },
     {
       type: "input",
       name: "about",
       message: "Provide some information about yourself",
+      when: ({confirmAbout}) => {
+          if (confirmAbout){
+              return true;
+          } else {
+              return false;
+          }
+      }
     },
   ]);
 };
@@ -34,12 +63,28 @@ const promptProject = (portfolioData) => {
       {
         type: "input",
         name: "name",
-        message: "What is the name of your project",
+        message: "What is the name of your project (Required)",
+        validate: nameInput => {
+            if (nameInput){
+                return true;
+            } else {
+                console.log('Please enter your project name!');
+                return false;
+            }
+        }
       },
       {
         type: "input",
         name: "description",
         message: "Provide a description of the project (Required)",
+        validate: descInput => {
+            if (descInput){
+                return true;
+            } else {
+                console.log('Please enter your project description!');
+                return false;
+            }
+        }
       },
       {
         type: "checkbox",
@@ -59,6 +104,14 @@ const promptProject = (portfolioData) => {
         type: "input",
         name: "link",
         message: "Enter the GitHub link to your project. (Required)",
+        validate: linkInput => {
+            if (linkInput){
+                return true;
+            } else {
+                console.log('Please enter your GitHub link!');
+                return false;
+            }
+        }
       },
       {
         type: "confirm",
