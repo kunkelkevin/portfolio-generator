1c85a1ab6141ee0d56a232342b7079c5a5c5e037
Kevin Kunkel <kunkelkevin@yahoo.com>
2021-05-10 21:11:39 -0500

    added projects to the html file

--- src/page-template.js
+++ src/page-template.js
@@ -1,6 +1,6 @@
-const generateAbout = aboutText => {
-  if(!aboutText){
-    return '';
+const generateAbout = (aboutText) => {
+  if (!aboutText) {
+    return "";
   }
   return `
   <section class="my-3" id="about">
@@ -8,11 +8,54 @@ const generateAbout = aboutText => {
     <p>${aboutText}</p>
   </section>
   `;
-}
+};
 
-const generatePage = templateData => {
+const generateProjects = (projectsArr) => {
+  return `
+      <section class="my-3" id="portfolio">
+        <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+        <div class="flex-row justify-space-between">
+        ${projectsArr
+          .filter(({ feature }) => feature)
+          .map(({ name, description, languages, link }) => {
+            return `
+            <div class="col-12 mb-2 bg-dark text-light p-3">
+              <h3 class="portfolio-item-title text-light">${name}</h3>
+              <h5 class="portfolio-languages">
+                Built With:
+                ${languages.join(", ")}
+              </h5>
+              <p>${description}</p>
+              <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+            </div>
+          `;
+          })
+          .join("")}
+  
+        ${projectsArr
+          .filter(({ feature }) => !feature)
+          .map(({ name, description, languages, link }) => {
+            return `
+            <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+              <h3 class="portfolio-item-title text-light">${name}</h3>
+              <h5 class="portfolio-languages">
+                Built With:
+                ${languages.join(", ")}
+              </h5>
+              <p>${description}</p>
+              <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+            </div>
+          `;
+          })
+          .join("")}
+        </div>
+      </section>
+    `;
+};
+
+const generatePage = (templateData) => {
   // console.log(templateData);
-  const {projects,about,...header} = templateData;
+  const { projects, about, ...header } = templateData;
 
   return `
       <!DOCTYPE html> 
@@ -31,7 +74,9 @@ const generatePage = templateData => {
       <body>
       <header>
         <div class="container flex-row justify-space-between align-center py-3">
-          <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
+          <h1 class="page-title text-secondary bg-dark py-2 px-3">${
+            header.name
+          }</h1>
           <nav class="flex-row">
             <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
               header.github
@@ -41,9 +86,12 @@ const generatePage = templateData => {
       </header>
       <main class="container my-5">
       ${generateAbout(about)}
+      ${generateProjects(projects)}
       </main>
       <footer class="container text-center py-3">
-        <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
+        <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${
+    header.name
+  }</h3>
       </footer>
     </body>
     </html>
